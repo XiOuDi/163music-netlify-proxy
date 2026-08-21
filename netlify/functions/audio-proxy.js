13d3a0f8fad0bd28eb1bdf9d4dfca63dd1191595
西欧帝 <l2892053356@gmail.com>
2026-08-21 21:33:26 +0800

    fix: 修复RSA公钥BigInt转换错误（移除前导零）

--- netlify/functions/audio-proxy.js
+++ netlify/functions/audio-proxy.js
@@ -17,8 +17,8 @@ const crypto = require('crypto');
 
 const AES_KEY = '0CoJUm6Qyw8W8jud';
 const AES_IV = Buffer.from('0102030405060708', 'utf8');
-const RSA_PUB_KEY = BigInt(
-  '00e0b509f6259df8642dbc35662901477df22677ec152b5ff68ace615bb7b725' +
+const RSA_PUB_KEY = BigInt('0x' +
+  'e0b509f6259df8642dbc35662901477df22677ec152b5ff68ace615bb7b725' +
   '152b3ab17a876aea8a5aa76d2e417629ec4ee341f56135fccf695280104e0312' +
   'ecbda92557c93870114af6c9d05c4f7f0c3685b7a46bee255932575cce10b424' +
   'd813cfe4875d3e82047b97ddef52741d546b8e289dc6935b3ece0462db0a22b8e7'
